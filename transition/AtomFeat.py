f61792cb85689eb5421d41953b45c063686c93f7
YuNan <yunan.hlju@gmail.com>
2018-04-01 20:29:00 +0800

    first commit

---
+++ transition/AtomFeat.py
@@ -0,0 +1,9 @@
+class AtomFeat:
+    def __init__(self):
+        self.s0 = -1
+        self.s1 = -1
+        self.s2 = -1
+        self.q0 = -1
+
+    def index(self):
+        return self.s0, self.s1, self.s2, self.q0
